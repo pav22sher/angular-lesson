c8a5cb131ec6e4536907c314a12b8d6c320988ca
scherbak <scherbak.pavel5@yandex.ru>
2022-03-08 12:23:30 +0400

    Убрал лишнее по своему мнению!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { AppComponent } from './app.component'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
